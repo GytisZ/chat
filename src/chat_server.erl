6da0fe6e5058574060125f65d6b202ff52927a53
Gytis Žilinskas <gytis.zilinskas@gmail.com>
2012-03-15 10:35:53 +0000

    version 1, only sign ins/outs and list. everything's still in one module

--- src/chat_server.erl
+++ src/chat_server.erl
@@ -2,36 +2,61 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, say_hello/0]).
+%% sort of public
+-export([start_link/0, sign_in/2, sign_out/2, list_names/1]).
 
+%% not so public
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+%% Server state
+-record(state, {name_list=gb_sets:new()}).
+
+
+%%% PUBLIC API
+
 start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+    gen_server:start_link(?MODULE, [], []).
+
+%% Try signing in. Server may refuse if the user name is taken
+sign_in(ServerRef, Nick) ->
+    gen_server:call(ServerRef, {sign_in, Nick}).
+
+%% Sign out. This is asynchronous till client will be separated from the server
+sign_out(ServerRef, Nick) ->
+    gen_server:cast(ServerRef, {sign_out, Nick}).
+
+%% Get the list of all the users currently connected to the server
+list_names(ServerRef) ->
+    gen_server:call(ServerRef, list_names).
+
+
+
+%%% Server functions
 
 init([]) ->
-    {ok, []}.
+    {ok, #state{name_list=gb_sets:empty()}}.
 
-say_hello() ->
-    gen_server:call(?MODULE, hello).
 
-%callbacks
-handle_call(hello, _From, State) ->
-    io:format("Hello from server!~n", []),
-    {reply, ok, State};
+handle_call({sign_in, Nick}, _From, State = #state{name_list=List}) ->
+    case gb_sets:is_element(Nick, List) of
+        false -> {reply, ok, State#state{name_list=gb_sets:add(Nick, List)}};
+        true  -> {reply, name_taken, State}
+    end;
+handle_call(list_names, _From, State=#state{name_list=List}) ->
+    {reply, gb_sets:to_list(List), State}.
 
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
 
-handle_cast(_Msg, State) ->
+handle_cast({sign_out, Nick}, State=#state{name_list=List}) ->
+    gb_sets:del_element(Nick, List),
+    {noreply, State#state{name_list=List}}.
+
+
+%% Placeholders for now
+handle_info(_Msg, State) ->
     {noreply, State}.
 
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-terminate(_Reason, _State) ->
+terminate(normal, _State) -> 
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
