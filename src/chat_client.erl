c5691ca9b2da922ece20fe0ad5e6766617951167
Gytis Žilinskas <gytis.zilinskas@gmail.com>
2012-03-17 12:36:43 +0000

    added new api functions to chat_client so that it's possible to give a custom name to the corresponding gen_server

--- src/chat_client.erl
+++ src/chat_client.erl
@@ -2,7 +2,8 @@
 -behaviour(gen_server).
 
 %% Public
--export([start/0, name/1, send/2, list_names/0, sign_out/0]).
+-export([start/0, start/1, name/1, name/2, send/2, list_names/0, sign_out/0,
+        sign_out/1]).
 
 %% Usual OTP goodness
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -17,9 +18,15 @@
 start() ->
     {ok, Pid} = gen_server:start({local, ?MODULE}, ?MODULE, [], []),
     set_pid(Pid).
+start(RefName) ->
+    {ok, Pid} = gen_server:start({local, RefName}, ?MODULE, [], []),
+    set_pid(RefName, Pid).
+
     
 name(Nick) ->
     gen_server:call(?MODULE, {sign_in, Nick}).
+name(RefName, Nick) ->
+    gen_server:call(RefName, {sign_in, Nick}).
 
 send(To, Message) ->
     chat_server:send_message(To, Message).
@@ -29,6 +36,8 @@ list_names() ->
 
 sign_out() ->
     gen_server:cast(?MODULE, sign_out).
+sign_out(RefName) ->
+    gen_server:cast(RefName, sign_out).
 %%%%%%%%%%%%%%%%%
 %%% CALLBACKS %%%
 %%%%%%%%%%%%%%%%%
@@ -83,3 +92,5 @@ code_change(_OldVsn, State, _Extra) ->
 
 set_pid(Pid) ->
     gen_server:cast(?MODULE, {set_pid, Pid}).
+set_pid(RefName, Pid) ->
+    gen_server:cast(RefName, {set_pid, Pid}).
